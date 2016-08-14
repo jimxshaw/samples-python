034734d3abd63da5465fe1e61be6e8e178c76b3a
Jim Shaw <jimxshaw@gmail.com>
2016-08-13 19:21:17 -0500

    added magic number game

--- introduction/guess.py
+++ introduction/guess.py
@@ -1,1 +1,30 @@
+import random
+
 print("Input your favorite number")
+
+number = input()
+
+print("Your favorite number is " + number)
+
+minNumber = 1
+maxNumber = 100
+
+magicNumber = random.randint(minNumber, maxNumber)
+
+message = "The magic number is within {0} and {1}, inclusive"
+print(message.format(minNumber, maxNumber))
+
+found = False
+
+while not found:
+    print("Guess the magic number!")
+    guess = int(input())
+    
+    if guess == magicNumber:
+        found = True
+    if guess < magicNumber:
+        print("Too low")
+    if guess > magicNumber:
+        print("Too high")
+
+print("You got it!")
