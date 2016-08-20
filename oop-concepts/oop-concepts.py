46f80d9301bb017e49aa7013bd87e5acbe82fd2f
Jim Shaw <jimxshaw@gmail.com>
2016-08-20 12:12:28 -0500

    Tested python object pointers.

--- oop-concepts/oop-concepts.py
+++ oop-concepts/oop-concepts.py
@@ -1,7 +1,14 @@
 class Person:
 
-    def say_hello(self):
-        print("Hello!")
+    def __init__(self, name):
+        self.name = name
 
-p1 = Person()
+    def say_hello(self):
+        print(id(self))
+        print("Hello, " + self.name)
+
+p1 = Person("Walter")
+p2 = Person("Alex")
+print(id(p1))
+print(id(p2))
 p1.say_hello()
