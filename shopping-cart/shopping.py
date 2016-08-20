5569d84e82767eb9d67e7fba0dfd842753e08ba7
Jim Shaw <jimxshaw@gmail.com>
2016-08-19 21:09:42 -0500

    Added comments.

--- shopping-cart/shopping.py
+++ shopping-cart/shopping.py
@@ -14,7 +14,11 @@ def go_shopping():
 def get_order():
     print("[command] [item] (command is a to add, d to delete, q to quit)")
     line = input()
+    # User input from start up to index 1.
     command = line[:1]
+    # User input from index 2 until the end of the input.
     item = line[2:]
+    # We return a tuple comprised of our command and item.
+    return (command, item)
 
 go_shopping()
