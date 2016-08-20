55406f8df53d809622a6e15e957aa6d950679bbd
Jim Shaw <jimxshaw@gmail.com>
2016-08-20 17:34:05 -0500

    Added Classroom class.

--- oop-concepts/oop-concepts.py
+++ oop-concepts/oop-concepts.py
@@ -7,8 +7,25 @@ class Person:
         print(id(self))
         print("Hello, " + self.name)
 
-p1 = Person("Walter")
-p2 = Person("Alex")
-print(id(p1))
-print(id(p2))
-p1.say_hello()
+
+class Classroom:
+
+    def __init__(self):
+        self._people = []
+
+    def add_person(self, person):
+        self._people.append(person)
+
+    def remove_person(self, person):
+        self._people.remove(person)
+
+    def greet(self):
+        for person in self._people:
+            person.say_hello()
+
+room = Classroom()
+room.add_person(Person("Jake"))
+room.add_person(Person("Rachel"))
+room.add_person(Person("Evan"))
+
+room.greet()
