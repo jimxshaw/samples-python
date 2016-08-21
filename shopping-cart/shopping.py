573d4abd14b4cb3671a0ee09202eb966982e3f3f
Jim Shaw <jimxshaw@gmail.com>
2016-08-20 19:10:08 -0500

    Refactor the entire app by removing functions and creating the Cart and Order class.

--- shopping-cart/shopping.py
+++ shopping-cart/shopping.py
@@ -1,52 +1,59 @@
+class Cart:
 
-def go_shopping():
-    cart = dict()
+    def __init__(self):
+        self._contents = dict()
 
-    while True:
-        order = get_order()
-        # If process_order returns False then not keyword will make it True.
-        if not process_order(order, cart):
-            break
+    def process(self, order):
+        if order.add:
+            # If the input item key is not in the cart, first set the value to 0
+            # then add 1 to the value. If there's already the same item in the
+            # cart then simply add 1 to the value.
+            if not order.item in self._contents:
+                self._contents[order.item] = 0
+            self._contents[order.item] += 1
+        elif order.delete:
+            # If given the command to delete an item and that item is in our dictionary,
+            # decrease its value by 1. If that item's value reaches 0 or below, completely
+            # remove that item key from the dictionary.
+            if order.item in self._contents:
+                self._contents[order.item] -= 1
+                if self._contents[order.item] <= 0:
+                    del self._contents[order.item]
+
+class Order:
     
-    print(cart)
-    print("Finished")
+    def __init__(self):
+        # By default our quit boolean is False meaning the app won't quit unless
+        # the user tells the app to quit. Also, initialize a few other attributes.
+        self.quit = False
+        self.add = False
+        self.delete = False
+        self.item = None
 
-def get_order():
-    print("[command] [item] (command is a to add, d to delete, q to quit)")
-    line = input()
-    # User input from start up to index 1.
-    command = line[:1]
-    # User input from index 2 until the end of the input.
-    item = line[2:]
-    # We return a tuple comprised of our command and item.
-    return (command, item)
+    def get_input(self):
+        print("[command] [item] (command is a to add, d to delete, q to quit)")
+        line = input()
+        # User input from start up to index 1.
+        command = line[:1]
+        # User input from index 2 until the end of the input.
+        self.item = line[2:]
 
-def process_order(order, cart):
-    # Unpack order, which is a tuple.
-    (command, item) = order
+        if command == "a":
+            self.add = True
+        elif command == "d":
+            self.delete = True
+        elif command == "q":
+            self.quit = True
 
-    if command == "a":
-        # Add item to cart.
-        add_to_cart(item, cart)
-    elif command == "d" and item in cart:
-        # Remove item from cart only if said item is in the cart.
-        delete_from_cart(item, cart)
-    elif command == "q":
-        return False
-    
-    return True
 
-def add_to_cart(item, cart):
-    # If the input item key is not in the cart, first set the value to 0
-    # then add 1 to the value. 
-    if not item in cart:
-        cart[item] = 0
-    cart[item] += 1
+cart = Cart()
+order = Order()
+order.get_input()
 
-def delete_from_cart(item, cart):
-    # If the item key is in the cart, remove 1 from the value but only if 
-    # the value is greater than 0. This is to prevent negative values.
-    if item in cart and cart[item] > 0:
-        cart[item] -= 1
 
-go_shopping()
+# As long as the quit boolean is false, we continue processing orders.
+while not order.quit:
+    cart.process(order)
+    order = Order()
+    order.get_input()    
+
