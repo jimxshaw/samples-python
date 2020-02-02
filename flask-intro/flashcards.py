cb2a83ab230bd969753bb8d49c5e3b51925909ac
Jim Shaw <jimxshaw@gmail.com>
2020-02-02 15:15:28 -0600

    Update welcome.html to show all cards.

--- flask-intro/flashcards.py
+++ flask-intro/flashcards.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html")
+    return render_template("welcome.html",
+                           cards=db)
 
 
 @app.route("/card/<int:index>")
