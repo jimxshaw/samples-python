c8bfe2e7e8a5b50d88cf408cf8763cbdd8a845e8
jimxshaw <jimxshaw@gmail.com>
2025-12-24 10:16:36 -0600

    Add an exercise.

---
+++ python-training/01hello.py
@@ -0,0 +1,10 @@
+# Exercise 2-1 (hello.py)
+
+# Using any editor, write a "Hello, world" python script.
+
+# Run the script explicitly
+
+# Run the script implicitly
+
+# Open the script in your IDE and run it from there.
+print("Hello, world")
