03a3f9f247e1742307a2e8940e9e712063784cc4
Jim Shaw <jimxshaw@gmail.com>
2026-05-14 14:28:18 -0500

    Add compound interest script.

--- rates/compount_interest.py
+++ rates/compount_interest.py
@@ -20,12 +20,26 @@ def compound(balance, rate, num_periods):
 # Remove the comment before the function definition and 
 # complete the function 
 
-#def compound_by_period(balance, rate, num_periods):
+def compound_by_period(balance, rate, num_periods):
     # 1. Create a list that is initialized with the first balance 
     # 2. Write a for loop. Inside the loop, compute the yearly balance and append 
     # to the list
     # 3. Return the list
+    result = [balance]
+
+    for n in range(0, num_periods):
+        bal = round(result[n] * (1 + rate), 2)
+        result.append(bal)
     
+    return result
+
+# Make sure both functions output the same thing except for
+# compound_by_period() returns a list.
+print()
+print(compound(balance, rate, 10))
+result_list = compound_by_period(balance, rate, 10)
+print(result_list)
+print()
 
 # Do NOT modify code below this line
 # -----------------------------
@@ -34,4 +48,6 @@ def change_per_period(balances):
          balances.append(balances[i+1] - balances[i])
     return balances
 
-
+# Make sure compound_by_period() works with the
+# change_per_period() function.
+print(change_per_period(result_list)) 
